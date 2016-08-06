226ac67e30589a8a5dee4e51b76123148e02a3fa
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-06 16:10:52 +0530

    added conteroller for table view

---
+++ TwitterTag/TwitterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TwitterTableViewCell.swift
+//  TwitterTag
+//
+//  Created by Rajhans Jadhao on 06/08/16.
+//  Copyright © 2016 Rajhans Jadhao. All rights reserved.
+//
+
+import UIKit
+
+class TwitterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
