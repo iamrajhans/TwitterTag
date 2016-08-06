430e6cfe3cf2f04650f9b865729325c5c103780c
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-06 15:36:59 +0530

    set constraint on custom uitablecells and added class for uitableclass

---
+++ TwitterTag/TwitterTag/TwitterTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterTableViewCell.swift
+//  TwitterTag
+//
+//  Created by Rajhans Jadhao on 06/08/16.
+//  Copyright © 2016 Rajhans Jadhao. All rights reserved.
+//
+
+import UIKit
+
+class TwitterTableViewCell: UITableViewCell {
+    
+}
